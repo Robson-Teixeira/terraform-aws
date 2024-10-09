476f243b34c406216b9a70435888bbf4775e5d76
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-10-08 23:22:04 -0300

    Organizando a configuração com a separação dos arquivos e aliases: Novos recursos em diferentes regiões

--- src/main.tf
+++ src/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
   count = 3 //Número de máquinas
   ami = "ami-0e86e20dae9224db8" //Amazon Machine Image
@@ -42,4 +48,15 @@ resource "aws_instance" "dev5" {
     Name = "dev5"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {  
+  provider = "aws.us-east-2"
+  ami = "ami-0ea3c35c5c3284d82"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
 }
--- src/security-group.tf
+++ src/security-group.tf
@@ -13,3 +13,20 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider = "aws.us-east-2"
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["ip1/porta", "ip2/porta"] #IP de saída (https://www.whatismyip.com/)
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
